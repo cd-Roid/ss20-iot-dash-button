23d22967506b4135b8d4bc4eefbd2d27b7ea3979
n-pola <n.polarek@live.de>
2020-06-25 12:11:55 +0200

    connect wlan, mqtt, display and navigation

--- esp32-IoT-Button/src/main.cpp
+++ esp32-IoT-Button/src/main.cpp
@@ -1,83 +1,158 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "credentials.h"
 #include "rgb_lcd.h"
 #include "paj7620.h"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 rgb_lcd lcd;
 const int switchPin = 33;
 static int hits = 0;
 int switchState = 0;
 int prevSwitchState = 0;
+int len = 0;
+StaticJsonDocument<256> doc;
+
+byte customChar[] = {
+    B00000,
+    B01010,
+    B00000,
+    B10001,
+    B10001,
+    B10001,
+    B01110,
+    B00000};
+
+/*** Initales verbinden mit WLAN ***/
+void setupWifi()
+{
+  Serial.print("\nConnecting to");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.println(WiFi.status());
+  }
+
+  Serial.print("\nConnected to");
+  Serial.println(ssid);
+}
+
+/*** Connenct/Reconnect to MQTT Broker in Case of Connection loss ***/
+const char *broker = "test.mosquitto.org";                          //Adresse des Brokers
+const char *inTopic = "thkoeln/IoT/bmw/montage/mittelkonsole/list"; //Ein Topic
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      client.subscribe(inTopic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+/*** Funktion welche ausgeführt wird, wenn eine Nachricht auf einem abbonierten Topic ankommt ***/
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Updating...");
+  Serial.print("Received messages: ");
+  Serial.println(topic);
+  for (int i = 0; i < length; i++)
+  {
+    Serial.printf("%c", (char)payload[i]); // Ausgabe der gesamten Nachricht
+  }
+  deserializeJson(doc, payload, length);
+  Serial.println(doc[0]["name"].as<char *>());
+  len = doc.size();
+  Serial.println(len);
+}
 
 void setup()
 {
+  Serial.begin(9600);
+  setupWifi();
+  client.setServer(broker, 1883);
+  client.setCallback(callback);
+
   lcd.begin(16, 2);
   pinMode(switchPin, OUTPUT);
-  Serial.begin(9600);
+
   lcd.setCursor(0, 0);
   lcd.print("Swipe 2 Navigate");
+  lcd.createChar(0, customChar);
   paj7620Init();
-  lcd.setCursor(0, 1);
-  lcd.print("Test");
-  // turn on the cursor:
-  lcd.blink();
 }
 
 void loop()
 {
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
   uint8_t data = 0; // Read Bank_0_Reg_0x43/0x44 for gesture result.
 
   paj7620ReadReg(0x43, 1, &data); // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
 
   if (data == GES_UP_FLAG)
-  { // When up gesture be detected,the variable 'data' will be set to GES_UP_FLAG.
-    digitalWrite(switchPin, HIGH);
-    hits = hits + 1;
-    delay(10); // turn the LED on (HIGH is the voltage level)
-  }
-
-  if (data == GES_DOWN_FLAG)
-  {                               // When down gesture be detected,the variable 'data' will be set to GES_DOWN_FLAG.
-    digitalWrite(switchPin, LOW); // turn the LED off by making the voltage LOW
-    hits = hits - 1;
+  {
+    if (hits + 1 < len)
+      hits = hits + 1;
+    else
+      hits = 0;
     delay(10);
   }
 
-  Serial.print("hits:");
-  Serial.println(hits);
-  if (hits == 1)
+  if (data == GES_DOWN_FLAG)
   {
-    Serial.println("Page 1");
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Page 1");
+    if (hits - 1 < 0)
+      hits = len - 1;
+    else
+      hits = hits - 1;
+    delay(10);
   }
-  else if (hits == 2)
-  {
-    Serial.println("Page 2");
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Page 2");
-  }
-  else
 
-      if (hits == 3)
-  {
-    Serial.println("Page 3");
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Page 3");
-  }
-  else
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(doc[hits]["name"].as<char *>());
+  lcd.setCursor(0, 1);
+  String quantity = doc[hits]["quantitiy"].as<char *>();
+  quantity += " St";
+  lcd.print(quantity);
+  lcd.write((unsigned char)0);
+  lcd.print("ck");
+  lcd.setCursor(13, 1);
+  lcd.print(hits + 1);
+  lcd.print("/");
+  lcd.print(len);
 
-      if (hits >= 4 || hits < 0)
-  {
-    hits = 1;
-    Serial.println("couter is reset");
-    Serial.println("Page 1");
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Page 1");
-  }
   delay(500);
 }
