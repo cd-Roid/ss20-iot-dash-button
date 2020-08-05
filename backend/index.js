fcff633bf3e3a35fef6cc221cf5a5a1b2b735612
George Iyawe <giyawe98@gmail.com>
2020-08-05 23:34:55 +0200

    added current topics

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,96 @@
-const app = require('express')();
-var http = require('http').createServer(app);
-const Mongoose = require('mongoose');
+const app = require("express")();
+var http = require("http").createServer(app);
+//const Mongoose = require('mongoose');
 const io = require("socket.io")(http);
-const mqtt = require('mqtt');
-let orderList = [];
+const mqtt = require("mqtt");
+let productList = [];
+let actionOrder = [];
+let orderedProduct = [];
 
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
- 
-client.on('connect', function () {
-  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/order/+', function (err) {
-  })
-  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/list', function (err) {
-  })
-})
- 
-client.on('message', function (topic, message) {
+var client = mqtt.connect("mqtt://hivemq.dock.moxd.io");
+
+client.on("connect", function () {
+  client.subscribe("mittelkonsole/order/+", function (err) {
+    console.error(err);
+  });
+  client.subscribe("thkoeln/IoT/bmw/montage/mittelkonsole/action/+", function (
+    err
+  ) {
+    console.error(err);
+  });
+  client.subscribe("thkoeln/IoT/bmw/montage/mittelkonsole/list", function (
+    err
+  ) {
+    console.error(err);
+  });
+  client.subscribe(
+    "thkoeln/IoT/bmw/montage/mittelkonsole/actionList",
+    function (err) {
+      console.error(err);
+    }
+  );
+  client.subscribe("thkoeln/IoT/bmw/montage/mittelkonsole/mode", function (
+    err
+  ) {
+    console.error(err);
+  });
+});
+
+client.on("message", function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
-  if (topic == "thkoeln/IoT/bmw/montage/mittelkonsole/list") {
-    orderList = JSON.parse(message.toString());
-    io.emit("orderList", orderList);
-  } else {
-    let msg = JSON.parse(message.toString())
-    io.emit("order", {msg, topic});
+  console.log(message.length);
+  if (message.length) {
+    let computedMessage = JSON.parse(message.toString());
+    console.log(topic);
+    console.log("Message:" + message.toString());
+    if (topic == "thkoeln/IoT/bmw/montage/mittelkonsole/list") {
+      io.emit("productList", computedMessage);
+    } else if (topic == "thkoeln/IoT/bmw/montage/mittelkonsole/actionList") {
+      io.emit("actionList", computedMessage);
+    } else if (topic == "thkoeln/IoT/bmw/montage/mittelkonsole/mode'") {
+      io.emit("mode", computedMessage);
+    } else if (
+      topic.startsWith("thkoeln/IoT/bmw/montage/mittelkonsole/action/")
+    ) {
+      actionOrder.push(computedMessage);
+      io.emit("orderedAction", computedMessage);
+    } else if (topic.startsWith("mittelkonsole/order/")) {
+      orderedProduct.push(computedMessage);
+      io.emit("orderedProduct", orderedProduct);
+    }
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+client.on("message", function(topic, message) {
+  if(message.length!=0){
+    if(topic == "thkoeln/IoT/bmw/montage/mittelkonsole/list"){
+      console.log("Topic:"+topic +",Message:"+ message );
+      client.publish(topic,message);
+      io.emit("AddedProduct", message);
+    }
+  }
+})
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('newProduct', (msg) => {
-    orderList.push(msg);
-    client.publish("thkoeln/IoT/bmw/montage/mittelkonsole/list", JSON.stringify(orderList), {retain: true})
-    console.log( msg);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("newProduct", (msg) => {
+    productList.push(msg);
+    client.publish(
+      "thkoeln/IoT/bmw/montage/mittelkonsole/list",
+      JSON.stringify(productList),
+      { retain: true }
+    );
+    console.log(msg);
   });
-  io.emit("orderList", orderList);
+  io.emit("productList", productList);
 });
 
 http.listen(3000, () => {
-  console.log('Server running');
+  console.log("Server running");
 });
