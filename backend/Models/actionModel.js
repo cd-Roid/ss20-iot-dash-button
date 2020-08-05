f8fd90b881e0fddc31223797f1a539b342cfd585
cd-Roid <giyawe98@gmail.com>
2020-08-06 00:13:18 +0200

    Implemented Model for actions

---
+++ backend/Models/actionModel.js
@@ -0,0 +1,12 @@
+const Mongoose = require('mongoose');
+
+const ActionModel = new Mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+module.exports = Mongoose.model('Action', ActionModel);
