73ea58cf8efd1b6eeb75aaf166af1614dbb1ee92
n-pola <n.polarek@live.de>
2020-08-06 10:34:15 +0200

    add Websocket connection

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
 import './plugins/bootstrap-vue';
+import VueSocketIO from 'vue-socket.io';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_',
+  },
+}));
 
 new Vue({
   router,
