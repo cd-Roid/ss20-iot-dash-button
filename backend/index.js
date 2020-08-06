c1cf9bb2ef486de9197e732725959429cfa7f9b5
cd-Roid <giyawe98@gmail.com>
2020-08-06 18:19:50 +0200

    Added changes from bad branch

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,20 @@ var http = require('http').createServer(app);
 const Mongoose = require('mongoose');
 const io = require('socket.io')(http);
 const mqtt = require('mqtt');
-
+const env = require("dotenv").config();
+const OrderedActions = require("./Models/orderedActionModel");
 const Actions = require('./Models/actionModel');
 const Mode = require('./Models/modeModel');
 const productList = require('./Models/productListModel');
 const Orders = require('./Models/orderModel.js');
+const setupID = require("./Models/setupIDModel");
 
-let actionOrder = [];
 let list = [];
-let orderedProduct = [];
+let setupMessage = 0;
 
 var client = mqtt.connect('mqtt://hivemq.dock.moxd.io');
 Mongoose.connect(
-  'mongodb+srv://IoT-admin:L7gmpz1U6wVPX4f9@iot.fyxl1.mongodb.net/IOT-DB?retryWrites=true&w=majority',
+  process.env.MONGODB_CONNECTION,
   { useUnifiedTopology: true, useNewUrlParser: true },
 ).catch((err) => console.error(err));
 const db = Mongoose.connection;
@@ -29,27 +30,28 @@ client.on('connect', function () {
   client.subscribe('mittelkonsole/order/+', function (err) {
     console.error(err);
   });
-  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/action/+', function (
-    err,
-  ) {
-    console.error(err);
-  });
-  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/list', function (
-    err,
-  ) {
-    console.error(err);
-  });
+  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/action/+',
+    function (err) {
+      console.error(err);
+    });
+  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/list',
+    function (err) {
+      console.error(err);
+    });
   client.subscribe(
     'thkoeln/IoT/bmw/montage/mittelkonsole/actionList',
     function (err) {
       console.error(err);
     },
   );
-  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/mode', function (
-    err,
-  ) {
+  client.subscribe('thkoeln/IoT/bmw/montage/mittelkonsole/mode',
+    function (err) {
+      console.error(err);
+    });
+
+  client.subscribe("thkoeln/IoT/setup", (err) => {
     console.error(err);
-  });
+  })
 });
 
 client.on('message', async function (topic, message) {
@@ -97,11 +99,24 @@ client.on('message', async function (topic, message) {
         }
       });
       io.emit('mode', computedMessage);
-    } else if (
-      topic.startsWith('thkoeln/IoT/bmw/montage/mittelkonsole/action/')
-    ) {
-      actionOrder.push(computedMessage);
+
+    } else if (topic.startsWith('thkoeln/IoT/bmw/montage/mittelkonsole/action/')) {
+      let eID = topic.split("/");
+      eID = eID[eID.length - 1];
+      let orderedAction = new OrderedActions({
+        name: computedMessage.name,
+        time: new Date(),
+        eID: eID
+      })
+      orderedAction.save((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Saved ${orderedAction} to db!`);
+        }
+      })
       io.emit('orderedAction', computedMessage);
+
     } else if (topic.startsWith('mittelkonsole/order/')) {
       let id = topic.split('/');
       id = id[id.length - 1];
@@ -109,6 +124,7 @@ client.on('message', async function (topic, message) {
         name: computedMessage.name,
         quantity: computedMessage.quantity,
         eID: id,
+        time: new Date()
       });
       await newOrder.save((err) => {
         if (err) {
@@ -119,11 +135,44 @@ client.on('message', async function (topic, message) {
       });
       io.emit('orderedProduct', newOrder);
     }
+    else if (topic == "thkoeln/IoT/setup") {
+      let setup = JSON.parse(message);
+      let newID = new setupID({ SetupId: setup });
+      setupMessage = setupID;
+      await newID.save((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Saved ${newID} to db!`);
+        }
+      });
+      io.emit('setupID', setup);
+    }
   }
 });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/setup", (req, res) => {
+  try {
+    res.send(setupMessage);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+app.post("/setup", (req, res) => {
+  try {
+    let randomID = setupMessage;
+    let eID = req.body.eID;
+    client.publish("thkoeln/IoT/setup/" + randomID, eID,
+      () => { console.log("Published id to new Device."); });
+    res.send({ message: "Device now registered under:" + eID });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
 app.get('/orders', async (req, res) => {
@@ -136,6 +185,18 @@ app.get('/orderList', async (req, res) => {
   let currentOrders = await productList.findOne({});
   console.log(currentOrders);
   res.send(currentOrders);
+});
+
+app.get('/orderedActions', async (req, res) => {
+  let orderedActions = await OrderedActions.find({});
+  console.log(orderedActions);
+  res.send(orderedActions);
+});
+
+app.get('/actions', async (req, res) => {
+  let actions = await Actions.find({});
+  console.log(actions);
+  res.send(actions);
 });
 
 io.on('connection', (socket) => {
