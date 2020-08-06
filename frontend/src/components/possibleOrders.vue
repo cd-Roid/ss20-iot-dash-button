ae3013cb45a0ae7bb36d9f955961e0ef441a02ac
n-pola <n.polarek@live.de>
2020-08-06 19:13:57 +0200

    add margin and timestamp

--- frontend/src/components/possibleOrders.vue
+++ frontend/src/components/possibleOrders.vue
@@ -14,7 +14,7 @@
 import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: 'OrderList',
+  name: 'possibleOrders',
   computed: {
     ...mapState({
       orders: (state) => state.orderList,
--- frontend/src/components/singleOrder.vue
+++ frontend/src/components/singleOrder.vue
@@ -1,12 +1,22 @@
 <template>
-  <b-card no-body>
+  <b-card no-body class="order">
     <b-card-body>
       <b-card-title>{{ order.name }}</b-card-title>
       <b-card-text>
         <p>Anzahl: {{ order.quantity }}</p>
         <p>Mitarbeiter: {{ order.eID }}</p>
       </b-card-text>
+        <b-button
+          href="#"
+          variant="primary"
+          :data-vue-id="order._id"
+          @click="dismiss(order._id)"
+          >Dismiss
+      </b-button>
     </b-card-body>
+    <template v-slot:footer>
+        <em>Vor {{timepassed}}</em>
+      </template>
   </b-card>
 </template>
 
@@ -16,9 +26,27 @@ export default {
   props: {
     order: Object,
   },
+  computed: {
+    timepassed() {
+      const currentTime = new Date();
+      const savedTime = new Date(this.order.time);
+      const timePassed = (currentTime - savedTime) / 1000;
+      let minutes = Math.floor((timePassed / 60) % 60);
+      let hours = Math.floor((timePassed / (100 * 60)) % 24);
+
+      hours = hours > 0 ? `${hours} Hours and` : '';
+
+      hours = hours < 10 && hours > 0 ? `0${hours}` : hours;
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+      return `${hours}  ${minutes} minutes`;
+    },
+  },
 };
 </script>
 
-<style>
-
+<style lang="scss">
+  .order {
+    margin: 10px 0;
+  }
 </style>
