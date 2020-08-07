3e1e97d6b6c6e393832e2a4fb414ef74d6fd8452
George Iyawe <giyawe98@gmail.com>
2020-08-07 20:47:36 +0200

    Added components for action

---
+++ frontend/src/components/ActionList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul class="action-list">
+      <li>
+          <Action v-for="action in actions" :key="action._id" :action="action">
+          </Action>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import Action from '@/components/singleAction.vue';
+
+export default {
+  name: 'ActionList',
+  components: {
+    Action,
+  },
+  computed: {
+    ...mapState({
+      actions: (state) => state.orderedActions,
+    }),
+  },
+  methods: {
+    ...mapActions(['loadOrderedAction']),
+  },
+  created() {
+    this.loadOrderedAction();
+  },
+};
+</script>
+
+<style>
+.action-list {
+  padding: 0;
+  list-style: none;
+}
+
+</style>
---
+++ frontend/src/components/AddProductModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-modal hide-backdrop v-model="showModel" title="Add Product">
+      <div>
+          <b-card 
+           text-variant="dark"
+            header="Add Product">
+            <p> Product Name</p>
+            <form @submit.prevent="">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text"
+                    model="name"
+                    name="name"
+                    class="form-control"/>
+                     <label for="stueck">Stück</label>
+                      <input type="number"
+                    model="stueck"
+                    name="stueck"
+                    class="form-control"/>
+                     <label for="step">Steps</label>
+                      <input type="number"
+                    model="step"
+                    name="step"
+                    class="form-control"/>
+                </div>
+                 <div class="form-group">
+                <button class="btn btn-primary">
+                  Add Product
+                </button>
+            </form>
+            </b-card>
+      </div>
+  </b-modal>
+</template>
+
+<script>
+// import { mapActions } from 'vuex';
+
+export default {
+  name: 'AddProduct',
+  data() {
+    return {
+      name: String,
+      stueck: Number,
+      steps: Number,
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
