122ba5b989a7c7041419d637106f16ce719165c2
George Iyawe <giyawe98@gmail.com>
2020-08-07 20:48:56 +0200

    added ActionList to show under orderedeActions

---
+++ frontend/src/components/possibleActions.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-container class="mt-3">
+    <h3>Actions</h3>
+    <ul>
+      <li>
+        <action v-for="action in actions" :key="action.name" :action="action"></action>
+      </li>
+    </ul>
+  </b-container>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import action from '@/components/Action.vue';
+
+export default {
+  name: 'possibleActions',
+  components: {
+    action,
+  },
+  computed: {
+    ...mapState({
+      actions: (state) => state.actionList.list,
+    }),
+  },
+  methods: {
+    ...mapActions(['loadActions']),
+  },
+  created() {
+    this.loadActions();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
--- frontend/src/views/ActionMode.vue
+++ frontend/src/views/ActionMode.vue
@@ -1,16 +1,20 @@
 <template>
-  <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-    <OrderList />
-  </b-jumbotron>
+  <b-jumbotron header="Actions" lead="Possible Actions">
+    <possibleActions></possibleActions>
+   <h2>Ordered Actions</h2>
+    <actionList></actionList>
+      </b-jumbotron>
 </template>
 
 <script>
-import OrderList from '@/components/OrderList.vue';
+import possibleActions from '@/components/possibleActions.vue';
+import actionList from '@/components/ActionList.vue';
 
 export default {
   name: 'OrderView',
   components: {
-    OrderList,
+    possibleActions,
+    actionList,
   },
   created() {
     // this.$socket.emit('mode_change', 1);
