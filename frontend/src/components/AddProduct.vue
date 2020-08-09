fe150b03216202641cb0c1a222dc2d24e96a5b9e
George Iyawe <giyawe98@gmail.com>
2020-08-09 17:51:25 +0200

    Form and action now use own component

---
+++ frontend/src/components/AddProduct.vue
@@ -0,0 +1,54 @@
+<template>
+        <div>
+            <b-card>
+            <h2> Add Product</h2>
+            <form @submit.prevent="">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text"
+                    v-model="product.name"
+                    name="name"
+                    class="form-control"/>
+                     <label for="stueck">Stück</label>
+                      <input type="number"
+                    v-model="product.quantity"
+                    name="stueck"
+                    class="form-control"/>
+                     <label for="step">Steps</label>
+                      <input type="number"
+                    v-model="product.step"
+                    name="step"
+                    class="form-control"/>
+                </div>
+                 <div class="form-group">
+                <button class="btn btn-primary" @click="addProduct(product)">
+                  Add Product
+                </button>
+                 </div>
+            </form>
+            </b-card>
+      </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      product: {
+        name: '',
+        quantity: 0,
+        step: 0,
+      },
+    };
+  },
+  methods: {
+    ...mapActions(['addProduct']),
+  },
+};
+</script>
+
+<style>
+
+</style>
