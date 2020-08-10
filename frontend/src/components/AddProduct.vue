294411bee13d2d1e164b3b1d6422cb39dd6104a8
n-pola <n.polarek@live.de>
2020-08-10 16:11:02 +0200

    clear Form on add Product and sent on form submit

--- frontend/src/components/AddProduct.vue
+++ frontend/src/components/AddProduct.vue
@@ -1,33 +1,22 @@
 <template>
-        <div>
-            <b-card>
-            <h2> Add Product</h2>
-            <form @submit.prevent="">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text"
-                    v-model="product.name"
-                    name="name"
-                    class="form-control"/>
-                     <label for="stueck">Stück</label>
-                      <input type="number"
-                    v-model="product.quantity"
-                    name="stueck"
-                    class="form-control"/>
-                     <label for="step">Steps</label>
-                      <input type="number"
-                    v-model="product.step"
-                    name="step"
-                    class="form-control"/>
-                </div>
-                 <div class="form-group">
-                <button class="btn btn-primary" @click="addProduct(product)">
-                  Add Product
-                </button>
-                 </div>
-            </form>
-            </b-card>
-      </div>
+  <div>
+    <b-card>
+      <h2>Add Product</h2>
+      <form @submit.prevent="sendProduct(product)">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" v-model="product.name" name="name" class="form-control" />
+          <label for="stueck">Stück</label>
+          <input type="number" v-model="product.quantity" name="stueck" class="form-control" />
+          <label for="step">Steps</label>
+          <input type="number" v-model="product.step" name="step" class="form-control" />
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary">Add Product</button>
+        </div>
+      </form>
+    </b-card>
+  </div>
 </template>
 
 <script>
@@ -38,17 +27,22 @@ export default {
     return {
       product: {
         name: '',
-        quantity: 0,
-        step: 0,
+        quantity: 1,
+        step: 1,
       },
     };
   },
   methods: {
     ...mapActions(['addProduct']),
+    sendProduct(product) {
+      this.addProduct(product);
+      this.product.name = '';
+      this.product.quantity = 1;
+      this.product.step = 1;
+    },
   },
 };
 </script>
 
 <style>
-
 </style>
